93a2b40c22baaa09f056646b61d88d44ccb7e012
Marquito0208 <129403206+Marquito0208@users.noreply.github.com>
2024-03-13 18:46:08 -0300

    Add files via upload

---
+++ futebol api/Controllers/JogadorController.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace futebol_api.Controllers
+{
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class JogadorController : ControllerBase
+    {
+        private static jogador teste = new jogador();
+
+        [HttpGet]
+        public ActionResult<jogador> Get()
+        {
+            return (teste);
+            Console.WriteLine("AAAAA FUNCIONA GITHUB");
+        }
+
+    }
+}
---
+++ futebol api/models/jogador.cs
@@ -0,0 +1,20 @@
+﻿namespace futebol_api.models
+{
+    public class jogador
+    {
+
+        public int id{ get; set; }
+
+        public string name { get; set; } = "cassio";
+
+        public int idade { get; set; } = 49;
+
+        public float altura { get; set; } = 1.96f;
+
+        public int numero { get; set; } = 12;
+
+
+
+
+    }
+}
